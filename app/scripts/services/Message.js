44b35bda1e0f1cf5d5bfd23398e803981e218c26
Adam Mohrbacher <adam.mohrbacher@gmail.com>
2017-08-06 14:51:21 -0600

    added functionality to message service and linked it via index.html

--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -2,9 +2,15 @@
   function Message($firebaseArray) {
     var Message = {};
     var ref = firebase.database().ref().child("messages");
-
+    
+    Message.getByRoomId = function(roomId) {
+        return $firebaseArray(ref.orderByChild('roomId').equalTo(roomId));
+    };
+      
     return Message;
-  }
+      
+    };
+}
 
   angular
     .module('blocChat')
