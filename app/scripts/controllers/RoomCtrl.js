4345de6d1b280576fe098b1448fd77708b89a9ba
Adam Mohrbacher <adam.mohrbacher@gmail.com>
2017-08-07 06:48:08 -0600

    wide array of changes

--- app/scripts/controllers/RoomCtrl.js
+++ app/scripts/controllers/RoomCtrl.js
@@ -1,9 +1,14 @@
 (function() {
-  function RoomCtrl(Room) {
+  function RoomCtrl(Room, Message) {
       this.all = Room.all;
-  }
-
+  } 
+  /**You establish the selected room with the following function. */
+  this.setRoom = function(clickedRoom){
+      this.selectedRoom = clickedRoom;
+      this.messages = Message.getByRoomId(this.selectedRoom.$id);
+  }   
+  
   angular
     .module('blocChat')
-    .controller('RoomCtrl', ["Room", RoomCtrl]);
+    .controller('RoomCtrl', ["Room", "Message", RoomCtrl]);
 })();
--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -2,15 +2,16 @@
   function Message($firebaseArray) {
     var Message = {};
     var ref = firebase.database().ref().child("messages");
-    
+    /**The following function takes the selected room and reveals messages for this room. orderByChild method reveals the messages logged in the firebase database of selected room. It's chained together with the child() method that is here represented by the variable ref. 
+    */
     Message.getByRoomId = function(roomId) {
-        return $firebaseArray(ref.orderByChild('roomId').equalTo(roomId));
+        return $firebaseArray(ref.orderByChild('roomId').equalTo('roomId'));
     };
-      
+    /**Return the array that now contains the messages of the selcted room.
+    */
     return Message;
       
-    };
-}
+    }
 
   angular
     .module('blocChat')
