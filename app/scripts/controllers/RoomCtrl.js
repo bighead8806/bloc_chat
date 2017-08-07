d77880ff3d31a0bc3f5c44df3497a2d103ea170c
Adam Mohrbacher <adam.mohrbacher@gmail.com>
2017-08-07 07:40:03 -0600

    work with Mike on 8/7 morning

--- app/scripts/controllers/RoomCtrl.js
+++ app/scripts/controllers/RoomCtrl.js
@@ -1,12 +1,13 @@
 (function() {
   function RoomCtrl(Room, Message) {
-      this.all = Room.all;
-  } 
-  /**You establish the selected room with the following function. */
-  this.setRoom = function(clickedRoom){
-      this.selectedRoom = clickedRoom;
-      this.messages = Message.getByRoomId(this.selectedRoom.$id);
-  }   
+      this.all = Room.all; 
+        /**You establish the selected room with the following function. */
+      this.setRoom = function(clickedRoom){
+          this.selectedRoom = clickedRoom;
+          this.messages = Message.getByRoomId(this.selectedRoom.$id);
+          console.log(this.messages);
+      };  
+  }
   
   angular
     .module('blocChat')
